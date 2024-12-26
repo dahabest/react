9f7da9b691a82a1b96755c17da03be5c7a8d04ca
iadaria <jadarya@mail.ru>
2024-12-26 22:13:31 +0400

    init translate

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const { writeFile } = require("node:fs/promises");
 const { getFileContent } = require("./libs/getFileContent");
-const { parseMarkdownFile } = require("./libs/parseMarkdownFile");
+const {
+  parseMarkdownFile,
+  parseMarkdownFileOld,
+  parseMarkdownForParagraph,
+} = require("./libs/parseMarkdownFile");
 
-async function readMdFile(fileName) {
+async function readMdFileOld(fileName) {
   try {
     const content = await getFileContent(fileName);
+    const paragraphs = await parseMarkdownForParagraph(content);
+    const result = await parseMarkdownFileOld(paragraphs);
 
-    const translated = await parseMarkdownFile(content);
-
-    //const fileWithTree = await writeFile("./output/tree2.md", translated);
+    await writeFile("./output/test.md", result);
   } catch (err) {
     console.log(err.message);
   }
 }
 
-readMdFile("./md/test.md");
+readMdFileOld("./md/test.md");
+
+async function readMdFile(fileName) {
+  try {
+    const content = await getFileContent(fileName);
+
+    const result = await parseMarkdownFile(content);
+
+    await writeFile("./output/tree1.md", result[0]);
+    await writeFile("./output/translated1.md", result[1]);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
--- libs/parseMarkdownFile.js
+++ libs/parseMarkdownFile.js
@@ -1,25 +1,113 @@
-const { markdown } = require("markdown");
+const { markdown: md } = require("markdown");
 const tags = require("./tags");
 
-const isPara = (node) =>
-  Array.isArray(node) && node[0].toLowerCase() === "para";
-const isHeader = (node) =>
-  Array.isArray(node) && node[0].toLowerCase().includes("header");
+TAGS = ["Intro", "YouWillLearn", "Diagram", "DeepDive"];
+OPEN_TAGS = TAGS.map((tag) => `<${tag}>`);
 
-const isTag = (node) => isPara(node) && String(node[1]).startsWith("<");
-const isOpenTag = (node) => isTag(node) && !String(node[1]).startsWith("</");
-const isCloseTag = (node) => isTag(node) && String(node[1]).startsWith("</");
+const rWord = /.*\w+/i;
+const isOpenTag = (paragraph) =>
+  TAGS.find((tag) => paragraph.includes(`<${tag}>`));
 
-const isOpenIntro = (node) => isOpenTag(node) && node[1].includes("Intro");
-const isCloseIntro = (node) => isCloseTag(node) && node[1].includes("Intro");
-const isDetails = (node) => isPara(node) && node[1].includes("details");
+const isCloseTag = (paragraph, tag) => {
+  //console.log({ paragraph, tag });
+  return paragraph.includes(`</${tag}>`);
+};
+
+async function parseMarkdownFileOld(paragraphsOld) {
+  let paragraphs = paragraphsOld.slice(0, 12);
+  //console.log(paragraphs);
+  let newFile = [];
+  let isStart = false;
+  let tag = null;
+  let toTranslate = [];
+  for (let key in paragraphs) {
+    let paragraph = paragraphs[key];
+    //console.log("interation", key, { isStart, tag, paragraph });
+    // is process and even not words
+    if (isStart) {
+      toTranslate.push(paragraph);
+    }
+
+    if (!isStart) {
+      newFile.push(paragraph);
+    }
+
+    if (isOpenTag(paragraph)) {
+      tag = isOpenTag(paragraph);
+      //console.log({ tag });
+      isStart = true;
+    }
+
+    // after adding a close tag(</Tag>) into the File (exclude key)
+    //console.log({ paragraph, tag, isCLose: isCloseTag(paragraph, tag) });
+    if (isCloseTag(paragraph, tag)) {
+      const closeTag = toTranslate.pop();
+      const translatedParagraphs = translateParagraphs(toTranslate);
+      const wrappedEngParagraphs = wrapEngParagraphs(toTranslate);
+      newFile = [
+        ...newFile,
+        ...translatedParagraphs,
+        ...wrappedEngParagraphs,
+        closeTag,
+      ];
+
+      isStart = false;
+      toTranslate = [];
+      tag = null;
+    }
+  }
+
+  // console.log("newFile", newFile);
+
+  return newFile;
+}
+//console.log(newFile);
+//newFile.push(ps[key]);
+//let isWord = ps[key].match(rWord);
+//const result = /(?<=^# .*?\n)([\s\S]*?)(?=\n<[^>]+>)/.exec(content);
+
+function wrapEngParagraphs(paragraphs) {
+  return [
+    "\r\n",
+    "<details>\r\n",
+    "<summary><small>(eng)</small></summary>\r\n",
+    ...paragraphs,
+    "</details>\r\n",
+    "\r\n",
+  ];
+}
+
+function translateParagraphs(paragraphs) {
+  return ["\r\n", "translated\r\n"];
+}
+
+module.exports = {
+  parseMarkdownFile,
+  parseMarkdownFileOld,
+  parseMarkdownForParagraph,
+};
+
+async function parseMarkdownForParagraph(content) {
+  let regex;
+
+  regex = /^#{1,6}\s?([^\n]+)/gm;
+  //paragraph
+  regex = /([^\n]+\n?)/g;
+
+  const result = content.match(regex);
+
+  return result;
+}
 
 async function parseMarkdownFile(content) {
-  const tree = markdown.parse(content);
+  const tree = md.parse(content);
 
+  let translatedTree = [];
   let toTranslate = [];
   let isStart = false;
+  let lines = "";
   for (const node of tree) {
+    translatedTree.push(node);
     // Идет процесс подготовки к переводу, пара и уже есть перевод - сброс
     if (isStart && isPara(node) && isDetails(node)) {
       toTranslate = [];
@@ -34,17 +122,41 @@ async function parseMarkdownFile(content) {
       isStart = false;
     }
 
-    if (isOpenIntro(node) && isHeader(node)) {
+    if (isOpenIntro(node) || isHeader(node)) {
       isStart = true;
     }
+    lines = lines + JSON.stringify(node) + "\n";
   }
 
-  console.log(toTranslate.map((node) => JSON.stringify(node)).join("\n"));
+  //console.log(toTranslate.map((node) => JSON.stringify(node)).join("\n"));
 
-  return lines;
+  let translated = translatedTree
+    .map((node) => JSON.stringify(node))
+    .join("\n");
+
+  console.log(md.renderJsonML(md.toHTMLTree(tree)));
+
+  return [lines, translated];
 }
-//lines = lines + JSON.stringify(node) + "\n";
 
-module.exports = {
-  parseMarkdownFile,
-};
+/* const isPara = (node) =>
+  Array.isArray(node) && node[0].toLowerCase() === "para";
+const isHeader = (node) =>
+  Array.isArray(node) && node[0].toLowerCase().includes("header");
+
+const isTag = (node) => isPara(node) && String(node[1]).startsWith("<");
+const isOpenTag = (node) => isTag(node) && !String(node[1]).startsWith("</");
+const isCloseTag = (node) => isTag(node) && String(node[1]).startsWith("</");
+
+const isOpenIntro = (node) => isOpenTag(node) && node[1].includes("Intro");
+const isCloseIntro = (node) => isCloseTag(node) && node[1].includes("Intro");
+const isDetails = (node) => isPara(node) && node[1].includes("details");
+
+regex = new RegExp(`(?<=>)([\\s\\S]*)(?=</YouWillLearn>)`, "gim");
+regex = new RegExp(`(?<=<YouWillLearn>)([\\s\\S]*)(?=</YouWillLearn>)`, "gim");
+regex = new RegExp(
+  `(?<=^#{1,3} .*\r?\n)([\\s\\S]*)(?=\r\n<Diagram name)`,
+  "gim"
+); */
+
+regex = /(?<=^#{1,3} .*\r?\n)([\s\S]*\n)(?=<Diagram)/gim;
